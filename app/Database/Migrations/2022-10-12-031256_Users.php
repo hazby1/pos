a1642e88cd455f853f2f1ec185dcec73f32ed18d
Ahmad Hasbi Asidik <hanizfoto@gmail.com>
2022-10-12 20:12:56 +0700

    Membuat migrasi tabel Users

--- app/Database/Migrations/2022-10-12-031256_Users.php
+++ app/Database/Migrations/2022-10-12-031256_Users.php
@@ -8,11 +8,52 @@ class Users extends Migration
 {
     public function up()
     {
-        //
+        // Membuat kolom/field untuk tabel users
+        $this->forge->addField([
+            'id_user' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'unique' => true
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ],
+            'level' => [
+                'type' => 'ENUM',
+                'constraint' => ['admin', 'kasir', 'teknisi'],
+                'default' => 'admin'
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ]
+        ]);
+
+        // Membuat primary key
+        $this->forge->addPrimaryKey('id_user', true);
+
+        // Membuat tabel Users
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        // Menghapus tabel Users
+        $this->forge->dropTable('users');
     }
 }
